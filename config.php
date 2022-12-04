c2a2a5dff4adfb1ad13a119736f750453e2257b4
Oleh <95093144+OlehKrupa@users.noreply.github.com>
2022-12-04 16:35:58 +0200

    Shop moved
    Remaining practical tasks

--- config.php
+++ config.php
@@ -1,4 +1,4 @@
 <?php
 define("ROOT_PATH", dirname(__FILE__));
 define("TEMPLATES_PATH", dirname(__FILE__).DIRECTORY_SEPARATOR."templates".DIRECTORY_SEPARATOR);
-?>
+session_start();
---
+++ db/product.sql
@@ -0,0 +1,22 @@
+INSERT INTO `product` (`id`, `product_name`, `price`, `category_id`) VALUES (NULL, 'RTX 3070', '30000', '1');
+INSERT INTO `product` (`id`, `product_name`, `price`, `category_id`) VALUES (NULL, 'RTX 3060', '20000', '1');
+INSERT INTO `product` (`id`, `product_name`, `price`, `category_id`) VALUES (NULL, 'RTX 3050', '15000', '1');
+INSERT INTO `product` (`id`, `product_name`, `price`, `category_id`) VALUES (NULL, 'RX 5700XT', '18000', '1');
+
+INSERT INTO `product` (`id`, `product_name`, `price`, `category_id`) VALUES (NULL, 'Ryzen 9 7950X', '27000', '2');
+INSERT INTO `product` (`id`, `product_name`, `price`, `category_id`) VALUES (NULL, 'Ryzen 5 7600X', '12000', '2');
+INSERT INTO `product` (`id`, `product_name`, `price`, `category_id`) VALUES (NULL, 'Core i5 12400f', '9000', '2');
+
+INSERT INTO `product` (`id`, `product_name`, `price`, `category_id`) VALUES (NULL, 'Kingston FURY DDR4-3200 16GB', '2000', '3');
+INSERT INTO `product` (`id`, `product_name`, `price`, `category_id`) VALUES (NULL, 'Kingston FURY DDR5-6000 16GB', '6000', '3');
+
+INSERT INTO `product` (`id`, `product_name`, `price`, `category_id`) VALUES (NULL, 'ASUS ROG CROSSHAIR X670E HERO sAM5', '28500', '4');
+INSERT INTO `product` (`id`, `product_name`, `price`, `category_id`) VALUES (NULL, 'Asus TUF Gaming B650M-Plus sAM5', '8700', '4');
+INSERT INTO `product` (`id`, `product_name`, `price`, `category_id`) VALUES (NULL, 'Asus TUF Gaming B660M-E s1700', '6000', '4');
+
+INSERT INTO `product` (`id`, `product_name`, `price`, `category_id`) VALUES (NULL, 'Chieftec Proton BDF-1000C 1000W', '5000', '5');
+INSERT INTO `product` (`id`, `product_name`, `price`, `category_id`) VALUES (NULL, 'Chieftec Proton BDF-750C 750W', '3700', '5');
+
+INSERT INTO `product` (`id`, `product_name`, `price`, `category_id`) VALUES (NULL, 'Kingston SSDNow A400 240GB 2.5', '980', '6');
+INSERT INTO `product` (`id`, `product_name`, `price`, `category_id`) VALUES (NULL, 'Western Digital Black SN770 NVMe SSD 1TB M.2 PCIe 4.0 x4', '4000', '6');
+INSERT INTO `product` (`id`, `product_name`, `price`, `category_id`) VALUES (NULL, 'Western Digital Blue 1TB 7200rpm', '1500', '6');
